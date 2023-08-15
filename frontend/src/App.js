94d571f946c499a8216f30b0e4f7d088c5f763bf
Rosie <collard.florian@yahoo.com>
2023-08-15 15:35:14 +0200

    Frontend ready

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './assets/logo.svg';
+import './styles/App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Hello World.
+				</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
