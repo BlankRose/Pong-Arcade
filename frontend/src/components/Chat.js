9f2aa762dba1b23c755a63fb5200624b5ed92e38
charles hajjar <chajjar@MacBook-Pro-de-charles.local>
2023-09-25 17:39:04 +0200

    test

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../styles/ChatPage.css';
 
 function ChatPage({ onLogout }) {
+    const [channels, setChannels] = useState([{ id: 1, name: "Channel 1" }, { id: 2, name: "Channel 2" }]);
+    const [selectedChannel, setSelectedChannel] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+
+    // Charger les messages lorsqu'un canal est sélectionné
+    useEffect(() => {
+        if (selectedChannel) {
+            // Remplacez par une requête fetch appropriée pour obtenir les messages du channel sélectionné
+            setMessages([
+                { id: 1, senderName: "User1", content: "Bonjour!" },
+                { id: 2, senderName: "User2", content: "Salut!" },
+            ]);
+        }
+    }, [selectedChannel]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (newMessage.trim()) {
+            // Remplacez par une requête fetch appropriée pour envoyer un nouveau message
+            setMessages([...messages, { id: messages.length + 1, senderName: "Moi", content: newMessage }]);
+            setNewMessage('');
+        }
+    };
+
     return (
-        <div>
+        <div className="chatPage">
             <h2>Messagerie</h2>
-            {/* ... (Votre composant de messagerie) */}
             <button onClick={onLogout}>Déconnexion</button>
+
+            <div className="chatContainer">
+                <div className="channelList">
+                    {channels.map(channel => (
+                        <div
+                            key={channel.id}
+                            onClick={() => setSelectedChannel(channel.id)}
+                            className={channel.id === selectedChannel ? 'selected' : ''}
+                        >
+                            {channel.name}
+                        </div>
+                    ))}
+                </div>
+                <div className="messageContainer">
+                    <div className="messageList">
+                        {messages.map(message => (
+                            <div key={message.id}>
+                                <strong>{message.senderName}:</strong> {message.content}
+                            </div>
+                        ))}
+                    </div>
+                    <form className="sendMessageForm" onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Tapez votre message..."
+                            value={newMessage}
+                            onChange={(e) => setNewMessage(e.target.value)}
+                        />
+                        <button type="submit">Envoyer</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
