202180e99224c40b12e4329fdc87b4c6786a499d
Maxime Fuhrmann <fuhrmann.maxime7@gmail.com>
2023-11-26 07:57:31 +0100

    history finished

--- frontend/src/components/HistoryItem.js
+++ frontend/src/components/HistoryItem.js
@@ -1,24 +1,11 @@
-import { useState, useEffect} from 'react';
-import apiHandle, { withAuth } from './API_Access';
 import { Link } from 'react-router-dom';
+import {useState} from 'react'
 import '../styles/HistoryItem.css'
 
 
-function HistoryItem({player1, player2, player1_score, player2_score, played_at, username}){
+function HistoryItem({player1, player2, player1_score, player2_score, played_at}){
 
 	const [ display, setDisplay ] = useState({display: 'none'});
-	const [user, setUser] = useState(null);
-
-	useEffect(() => {
-		apiHandle.get(`/users/${username ||'me'}`, withAuth() )
-			.then(response => {
-				setUser(response.data);
-			})
-			.catch(err => {
-				console.error(err);
-			});
-	}, [username])
-
 	const date = new Date(played_at); // Convert to Date object
 	const min_ago = Math.floor((Date.now() - date.getTime()) / 60000);
 	const hour_ago = Math.floor((Date.now() - date.getTime()) / 3600000);
@@ -33,19 +20,6 @@ function HistoryItem({player1, player2, player1_score, player2_score, played_at,
 			{player1_score < 0 ? 'Gave Up' : player1_score} : {' '}
 			<Link className='link-name' to={`/profile/${player2?.id}`}>{player2?.username}</Link> {' '}
 			{player2_score < 0 ? 'Gave Up' : player2_score}
-			{(player1_score > player2_score && player1?.username === user?.username )
-			|| (player2_score > player1_score && player2?.username === user?.username) ?
-			 (!username &&  <span className='victory'>Victoire</span>)
-			 ||	
-				<span className='defeat'>   Defaite</span>		 			
-			:
-				(!username && <span className='defeat'>   Defaite</span>)
-				||
-					<span className='victory'>Victoire</span>
-
-			
-			
-			}
 			<div className='history-date' style={display}>
 				Played on: {date.toLocaleString()} ({
 					min_ago <= 0
