e6f61ccafe65e72b1bd4a2a858b708b6d61cb87c
Rosie <collard.florian@yahoo.com>
2024-01-09 11:39:37 +0100

    Anti-Flicker

--- frontend/src/Loader.js
+++ frontend/src/Loader.js
@@ -1,9 +1,15 @@
 import store from './store/index.js'
 import apiHandle, { withAuth } from './components/API_Access.js'
 import userSlice from './store/user.js';
-import { useSelector } from 'react-redux';
 
-export async function statusLoader({ connectSockets, disconnectSockets }) {
+const key_name = "lastAttempt";
+
+export async function statusLoader({ connectSockets, disconnectSockets })
+{
+	// ANTI-FLICKERING: ALLOW TIME FOR BACKEND TO REGISTER CORRECTLY
+	const last = parseInt(localStorage.getItem(key_name)) || 0;
+	if (Date.now() < last + 500) // 500 MS
+		return;
 
 	// const userStatus = useSelector(state=> state.user.status)
     // console.log("userstatus: ", userStatus)
@@ -14,16 +20,16 @@ export async function statusLoader({ connectSockets, disconnectSockets }) {
 			if (res.data === 'online') {
 				connectSockets();
 				store.dispatch(userSlice.actions.setOnline())
+				localStorage.setItem(key_name, Date.now().toString());
 			} else {
 				disconnectSockets();
-				store.dispatch(userSlice.actions.setOffline())
+				store.dispatch(userSlice.actions.setOffline());
+				localStorage.removeItem(key_name);
 			}
 		})
 		.catch(err => {
 			console.warn(err.response);
 		});
-	return null;
-
 }
 
 
