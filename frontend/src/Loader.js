83a0c0c6c1deeb27959256cef480b635b6306000
Maxime Fuhrmann <mfuhrman@k2r1p10.42mulhouse.fr>
2024-01-10 13:20:39 +0100

    help

--- frontend/src/Loader.js
+++ frontend/src/Loader.js
@@ -6,25 +6,18 @@ const key_name = "lastAttempt";
 
 export async function statusLoader({ connectSockets, disconnectSockets })
 {
-	// ANTI-FLICKERING: ALLOW TIME FOR BACKEND TO REGISTER CORRECTLY
-	const last = parseInt(localStorage.getItem(key_name)) || 0;
-	if (Date.now() < last + 500) // 500 MS
-		return null;
-
 	// const userStatus = useSelector(state=> state.user.status)
     // console.log("userstatus: ", userStatus)
     console.log("statusLoader")
 	apiHandle.get('/auth/loginStatus', withAuth())
 		.then(res => {
             console.log("res: ", res)
-			if (res.data === 'online') {
+			if (res.data !== 'offline') {
 				connectSockets();
 				store.dispatch(userSlice.actions.setOnline())
-				localStorage.setItem(key_name, Date.now().toString());
 			} else {
 				disconnectSockets();
 				store.dispatch(userSlice.actions.setOffline());
-				localStorage.removeItem(key_name);
 			}
 		})
 		.catch(err => {
